1fc0397829d0a66fc122d6436866c75135fc8e2e
dev-nuII <tristan.feyerherm@icloud.com>
2026-09-08 23:26:40 -0500

    Add method check for load-save API handler

--- api/load-save.js
+++ api/load-save.js
@@ -1,8 +1,16 @@
 const db = require("./firebase");
 
 module.exports = async function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).json({
+      error: "Method not allowed"
+    });
+  }
+
   try {
-    const snapshot = await db.ref("lilguy_var0").once("value");
+    const snapshot = await db
+      .ref("lilguy_var0")
+      .once("value");
 
     return res.status(200).json({
       save: snapshot.val()
